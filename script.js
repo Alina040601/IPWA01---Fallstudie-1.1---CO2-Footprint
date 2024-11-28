f6ec70dee9ac62517e69dbdd3970737e714c87b9
Alina Bach <al.ba9424@gmail.com>
2024-11-28 13:31:12 +0100

    started rtl responsiveness of ofcanvas menu in script.js

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const offcanvas = document.getElementById("menu");
+window.onload = onPageload;
 
-const menuButton = document.getElementById("offcanvas-btn");
-    menuButton.addEventListener("click", menuClick);
+function onPageload() {
+    function getLanguage() {
+        const language = navigator.language;
+        return language;
+    }
 
-function menuClick() {
-        console.log("menu clicked");
+    const userLanguage = getLanguage();
+    console.log("Browser language is set to:", userLanguage);
+
+if (userLanguage === "de-DE") {
+    changeCSSVariable();
 }
 
-const language = navigator.language;
-console.log(language);
-function getLanguage() {
-    navigator.language;
-    returnLanguage();
-    function returnLanguage() {
-        if (language === "de-DE") {
-            console.log("Deutsch");
-        }
+else {
+    console.log("No changes made to CSS variables");
+
+}
+}
+
+const offcanvasStart = document.querySelector(".offcanvas.offcanvas-start");
+const offcanvasStartShow = document.querySelector(".offcanvas.offcanvas-start.show");
+const offcanvasStartShowing = document.querySelector(".offcanvas.offcanvas-start.showing");
+const offcanvasStartHidig = document.querySelector(".offcanvas.offcanvas-start.hiding");
+
+function changeCSSVariable() {
+    if (offcanvasStart) {
+        offcanvasStart.style.right = "0";
+        offcanvasStart.style.transform = "translateX(+100%)";
+        offcanvasStart.style.transition = "ease-in-out 0.5s";
+    }
+    if (offcanvasStartShow) {
+        offcanvasStartShow.style.transform = "translateX(0)";
+    }
+    if (offcanvasStartShowing) {
+        offcanvasStartShowing.style.transform = "translateX(0)";
+    }
+    if (offcanvasStartHidig) {
+        offcanvasStartHidig.style.transform = "translateX(+100%)";
     }
 }
