36c62eafd482c8034461c64f5bf98978967ee496
Alina Bach <al.ba9424@gmail.com>
2024-11-29 12:12:19 +0100

    actually finished privacy.html. noticed that the OR operator was not working as expected in script.js. created a constant with an array of languages and fixed the issue.

--- script.js
+++ script.js
@@ -9,15 +9,24 @@ function onPageload() {
     const userLanguage = getLanguage();
     console.log("Browser language is set to:", userLanguage);
 
-        if (userLanguage === "ar || ar-DZ || ar-BH || ar-TD || ar-KM || ar-DJ || ar-EG || ar-ER || ar-IQ || ar-IL || ar-JO || ar-KW || ar-LB || ar-LY || ar-MR || ar-MA || ar-OM || ar-PS || ar-QA || ar-SA || ar-SO || ar-SS || ar-SD || ar-SY || ar-TN || ar-AE || ar-EH || ar-YE ||ff || ff-GN || ff-CM || ff-MR || ff-SN || he || he-IL || bm || bm-Latn || hu || hu-HU || syr || aii || cld || mid || ku || ku-IR || ug || ug-CN || sd || sd-PK || sd-IN || ks || ks-IN || ps || ps-AF || ps-PK || dv || dv-MV") {
-            const offcanvasButton = document.getElementById('offcanvas-btn')
-            const offcanvasButtonRTL = document.getElementById('offcanvas-btn-rtl')
-            offcanvasButton.style.visibility = 'hidden';
-            offcanvasButtonRTL.style.visibility = 'visible';
-        }
+    const rtlLanguages = [
+        "de-DE", "ar", "ar-DZ", "ar-BH", "ar-TD", "ar-KM", "ar-DJ", "ar-EG", "ar-ER", "ar-IQ", 
+        "ar-IL", "ar-JO", "ar-KW", "ar-LB", "ar-LY", "ar-MR", "ar-MA", "ar-OM", 
+        "ar-PS", "ar-QA", "ar-SA", "ar-SO", "ar-SS", "ar-SD", "ar-SY", "ar-TN", 
+        "ar-AE", "ar-EH", "ar-YE", "he", "he-IL", "ff", "ff-GN", "ff-CM", "ff-MR", 
+        "ff-SN", "bm", "bm-Latn", "hu", "hu-HU", "syr", "aii", "cld", "mid", "ku", 
+        "ku-IR", "ug", "ug-CN", "sd", "sd-PK", "sd-IN", "ks", "ks-IN", "ps", "ps-AF", 
+        "ps-PK", "dv", "dv-MV"
+    ];
 
-        else {
-            console.log('dir="ltr"');
-
-        }
+    if (rtlLanguages.includes(userLanguage)) {
+        const offcanvasButton = document.getElementById('offcanvas-btn');
+        const offcanvasButtonRTL = document.getElementById('offcanvas-btn-rtl');
+        offcanvasButton.style.visibility = 'hidden';
+        offcanvasButtonRTL.style.visibility = 'visible';
+        document.documentElement.setAttribute('dir', 'rtl');
+        console.log('dir="rtl"');
+    } else {
+        console.log('dir="ltr"');
+    }
 }
