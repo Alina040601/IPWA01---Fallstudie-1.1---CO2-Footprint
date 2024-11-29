777c015cb17129383ffa312d1711cf691aa537d9
Alina Bach <al.ba9424@gmail.com>
2024-11-29 10:21:32 +0100

    Added second offcanvas to the right side of the page with a button that only appears if certain languages are set.

--- script.js
+++ script.js
@@ -9,49 +9,15 @@ function onPageload() {
     const userLanguage = getLanguage();
     console.log("Browser language is set to:", userLanguage);
 
-        if (userLanguage === "de-DE") {
-            changeCSSVariable();
+        if (userLanguage === "en-EN") {
+            const offcanvasButton = document.getElementById('offcanvas-btn')
+            const offcanvasButtonRTL = document.getElementById('offcanvas-btn-rtl')
+            offcanvasButton.style.visibility = 'hidden';
+            offcanvasButtonRTL.style.visibility = 'visible';
         }
 
         else {
-            console.log("No changes made to CSS variables");
+            console.log('dir="ltr"');
 
         }
 }
-
-const offcanvasStart = document.querySelector('.offcanvas.offcanvas-start');
-const offcanvasStartShow = document.querySelector('.offcanvas.offcanvas-start.show');
-const offcanvasStartShowing = document.querySelector('.offcanvas.offcanvas-start.showing');
-const offcanvasStartHiding = document.querySelector('offcanvas.offcanvas-start.show.hiding');
-
-function changeCSSVariable() {
-    
-        offcanvasStart.style.right = "0";
-        offcanvasStart.style.transform = "translateX(+100%)";
-
-        offcanvasStartShowing.style.transform = "translateX(0)";    
-
-        offcanvasStartShow.style.transform = "translateX(0)";
-
-        offcanvasStartHiding.style.transform = "translateX(+100%)";
-    }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const offcanvasElement = document.querySelector('.offcanvas.offcanvas-start');
-
-    if (offcanvasStartShow) {
-        // Listen for when the offcanvas is fully shown
-        offcanvasStartShow.addEventListener('shown.bs.offcanvas', () => {
-            console.log('Offcanvas is now shown.');
-            offcanvasStartShow.style.transform = 'translateX(0)';
-        });
-
-        // Listen for when the offcanvas is fully hidden
-        offcanvasStartHiding.addEventListener('hidden.bs.offcanvas', () => {
-            console.log('Offcanvas is now hidden.');
-            offcanvasStartHiding.style.transform = 'translateX(+100%)';
-        });
-    }
-});    
-
-const hn = ".bs.modal"
